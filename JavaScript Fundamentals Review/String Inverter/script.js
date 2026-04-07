9273aef34121409a99caaf28097bb96a29d01fa5
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2026-04-07 12:53:19 +0200

    Create script.js

---
+++ JavaScript Fundamentals Review/String Inverter/script.js
@@ -0,0 +1,8 @@
+/* Build a String Inverter
+In this lab, you will build a simple string inverter that reverses the characters of a given string.
+
+For example, "hello" should become "olleh". */
+
+function reverseString(stri){
+  return stri.split("").reverse().join("");
+}
